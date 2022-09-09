aac4f6504e8661134ca8f639c9d8acc74754ee71
Dylan <BooshBear03@gmail.com>
2022-09-09 16:49:36 -0700

    Week 1 finished

---
+++ Week1/kata.py
@@ -0,0 +1,19 @@
+
+
+def theFunction(arg):
+    switch = {
+        1:"1",
+        2:"2",
+        3:"Pepsi",
+        5:"Coke",
+    }
+    if arg % 3 == 0:
+        switch.update({arg: "Pepsi"})
+    if arg % 5 == 0:
+        switch.update({arg: "Coke"})
+    if arg % 5 == 0 and arg % 3 == 0:
+        switch.update({arg: "PepsiCoke"})
+    return switch.get(arg, "Nothing")
+
+thearg = int(input("choose a number: "))
+print(theFunction(thearg))
