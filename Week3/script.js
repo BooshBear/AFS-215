26d38cad2043880654b168c41bb17ac4f3377698
Dylan <BooshBear03@gmail.com>
2022-09-24 12:45:34 -0700

    Fix2

--- Week3/script.js
+++ Week3/script.js
@@ -1,3 +1,5 @@
+
+
 module.exports = class User {
     constructor(num1, num2, name = 'dylan', age = '18') {
         this.num1 = num1
@@ -9,7 +11,12 @@ module.exports = class User {
         return `Hello ${this.name} (age ${this.age}) this is my code.`
     }
     math(){
-        return this.num1 + this.num2
+        return [
+            this.num1 + this.num2,
+            this.num1 - this.num2,
+            this.num1 * this.num2,
+            this.num1 / this.num2
+        ]
     }
     other(){
         return `${this.num1}` + `${this.num2}`
